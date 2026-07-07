dcf4ee8b96de5da802512d26503a1a9a72f88e7e
Henri Ortiz Campos <your_github_email@example.com>
2026-07-07 02:02:06 -0500

    Connect tickets UI to ASP.NET API

--- client/src/services/ticketService.js
+++ client/src/services/ticketService.js
@@ -6,3 +6,8 @@ export async function getTickets() {
   const response = await axios.get(API_URL)
   return response.data
 }
+
+export async function createTicket(ticket) {
+  const response = await axios.post(API_URL, ticket)
+  return response.data
+}
